2e21080b3f53501e11d2a2cf8324b0998d360c64
ruizherrero1 <ruizherrero1@gmail.com>
2026-05-18 17:58:26 +0200

    Configura Google Drive OAuth para recetario

--- firebase-config.js
+++ firebase-config.js
@@ -7,3 +7,8 @@ window.RECETARIO_FIREBASE_CONFIG = {
   appId: "1:137712549509:web:4c13d8eb6825ae650633ec",
   measurementId: "G-PD9MHMKKEM"
 };
+
+window.RECETARIO_DRIVE_CONFIG = {
+  folderId: "1iOIk142KsM7QitqasUgFItlOTbpk16yc",
+  clientId: "137712549509-6ht0t0rtkl9raks82husqd6mni3m9dtn.apps.googleusercontent.com"
+};
