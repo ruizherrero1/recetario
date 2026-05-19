28c32908a27bcfa6d7bbf9c596f9b967f150a22b
ruizherrero1 <ruizherrero1@gmail.com>
2026-05-19 18:15:34 +0200

    Remove Firebase config and keep Drive settings

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,3 @@
-window.RECETARIO_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBADC7lRWcvD_42hqskKkXXNiiNnMd4gvE",
-  authDomain: "recetas-15610.firebaseapp.com",
-  projectId: "recetas-15610",
-  storageBucket: "recetas-15610.firebasestorage.app",
-  messagingSenderId: "137712549509",
-  appId: "1:137712549509:web:4c13d8eb6825ae650633ec",
-  measurementId: "G-PD9MHMKKEM"
-};
-
 window.RECETARIO_DRIVE_CONFIG = {
   folderId: "1iOIk142KsM7QitqasUgFItlOTbpk16yc",
   clientId: "137712549509-6ht0t0rtkl9raks82husqd6mni3m9dtn.apps.googleusercontent.com"
